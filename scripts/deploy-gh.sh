432dfc454a0a918465270589dc8f3b497e39b1bb
逍遥子 <74635349+Epic-Deno@users.noreply.github.com>
2021-09-21 02:50:54 +0800

    更新配置

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -23,6 +23,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:Epic-Deno/code-review.git master:gh-pages
+git push -f https://github.com/Epic-Deno/code-review.git master:gh-pages
 
 cd -
